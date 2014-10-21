e65064126c04002d92e5a58f145c55357763d65d
Vincent Cardillo <vcardillo@gmail.com>
2014-10-20 18:38:48 -0700

    Wrapping up detail view of movies.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ generalassembly-wdi-challenge
 =============================
 
 Javascript Single Page App. Code challenge for GeneralAssembly.
+
+I wrote this app using yeoman and the angular generator. This was mostly to demonstrate use of an advanced tool. However I mainly stuck to simple JS and jquery for the application code itself.
+
++ Make sure to have an updated npm: `npm update -g npm`
++ cd into the top directory, and run `grunt serve`.
++ That should be it!
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -55,7 +55,7 @@ var omdb = (function() {
     $('#search-results-items').empty();
 
     $("head").append('<script src="'+src+'"></script>');
-  }
+  };
 
 
   /**
@@ -84,29 +84,65 @@ var omdb = (function() {
 
         domItem.on("click", function() {
           var imdbID = $(this).attr("imdbID");
-          that.getDetails(imdbID);
+          that.getDetails(imdbID, this);
         });
 
         $('#search-results-items').append(domItem);
       }
     }
     // No results found
-    else if (data.hasOwnProperty('Response') && data.Response == "False") {
+    else if (data.hasOwnProperty('Response') && data.Response == 'False') {
       $('#search-results-items').html('<span class="label label-info">'+data.Error+'</span>');
     }
     // Some other yet-unknown case
     else {
-      $('#search-results-items').text("An unknown error has occurred (panic now).");
+      $('#search-results-items').text('An unknown error has occurred (panic now).');
     }
-  }
+  };
 
 
   /**
    * Get movie details
    */
-  that.getDetails = function(id) {
-    console.log(id);
-  }
+  that.getDetails = function(id, movie) {
+    //console.log(id);
+    $(movie).find(".search-result-detail").removeClass('hidden');
+    $(movie).css("cursor", "default");
+
+    var src = "http://www.omdbapi.com/?i="+id+"&callback=omdb.detailSuccess";
+    $("head").append('<script src="'+src+'"></script>');
+  };
+
+
+  /**
+   * Get movie details success
+   */
+  that.detailSuccess = function(movieDetails) {
+    //console.log(movieDetails);
+    var movie = $("[imdbid='"+movieDetails.imdbID+"']"); // Find the movie in the dom
+    var detail = $(movie).find('.result-detail'); // Find its detail container in the dom
+
+    // Populate with data
+    $(detail).find('img.mv-poster').attr("src", movieDetails.Poster);
+    $(detail).find('i.mv-rated').text(movieDetails.Rated);
+    $(detail).find('i.mv-released').text(movieDetails.Released);
+    $(detail).find('i.mv-runtime').text(movieDetails.Runtime);
+    $(detail).find('i.mv-language').text(movieDetails.Language);
+    $(detail).find('i.mv-genre').text(movieDetails.Genre);
+    $(detail).find('i.mv-director').text(movieDetails.Director);
+    $(detail).find('i.mv-writer').text(movieDetails.Writer);
+    $(detail).find('i.mv-actors').text(movieDetails.Actors);
+    $(detail).find('i.mv-plot').text(movieDetails.Plot);
+    $(detail).find('i.mv-country').text(movieDetails.Country);
+    $(detail).find('i.mv-awards').text(movieDetails.Awards);
+    $(detail).find('i.mv-metascore').text(movieDetails.Metascore);
+    $(detail).find('i.mv-imdbRating').text(movieDetails.imdbRating);
+    $(detail).find('i.mv-imdbVotes').text(movieDetails.imdbVotes);
+
+    $(movie).find('p.result-detail-loader').remove();
+    detail.removeClass('hidden');
+  };
+
 
   return that;
 })();
